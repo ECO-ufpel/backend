7f97e8b6afb4f2d55be3cdf843307d916d366287
Gustavo Roos <gustavohroos@gmail.com>
2023-11-28 14:21:36 -0300

    initial commit

---
+++ src/main/java/com/ecoufpel/ecoufpelapp/EcoufpelAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecoufpel.ecoufpelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoufpelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoufpelAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoufpel/ecoufpelapp/EcoufpelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoufpel.ecoufpelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoufpelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
