78e9b1c1c9ce5bd44fee6a6cdc9dc7d3870c8458
JelsonRodrigues <jelsonrodrigues25@gmail.com>
2024-02-28 20:13:34 -0300

    feat: add query to return User by cpf

--- src/main/java/com/ecoufpel/ecoufpelapp/repositories/UserRepository.java
+++ src/main/java/com/ecoufpel/ecoufpelapp/repositories/UserRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<UserDetails> findBycpf(String cpf);
     Optional<UserDetails> findByRegistration(String registration);
-
     Optional<UserDetails> findByCpfOrRegistration(String cpf, String registration);
+    Optional<User> findUserByCpf(String cpf);
 }
