4efb56959ce340dd0856656ceecfca2c61ce12ed
Allan <allan20.1@hotmail.com>
2024-03-01 15:33:37 -0300

    fix: logic to verify database on a fixed rate and send to WS

--- src/main/java/com/ecoufpel/ecoufpelapp/services/GetConsumptionDataService.java
+++ src/main/java/com/ecoufpel/ecoufpelapp/services/GetConsumptionDataService.java
@@ -7,25 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ecoufpel.ecoufpelapp.repositories.DataConsumptionRepository;
 import java.sql.Timestamp;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 @Service
 public class GetConsumptionDataService {
 
-    public static final int MIN_INTERVAL_VALUE = 3000;
-    public static final int MAX_INTERVAL_VALUE = 10000;
+    public static final int VERIFY_DATABASE_RATE = 5000;
 
-    @Autowired
-    private WebSocketEventListener webSocketEventListener;
+    private final WebSocketEventListener webSocketEventListener;
 
-    @Autowired
-    DataConsumptionRepository dataConsumptionRepository;
+    private final DataConsumptionRepository dataConsumptionRepository;
     private Timestamp lastTimeStampChecked;
     @Autowired
-    public GetConsumptionDataService(WebSocketEventListener webSocketEventListener) {
+    public GetConsumptionDataService(WebSocketEventListener webSocketEventListener, DataConsumptionRepository dataConsumptionRepository) {
         this.webSocketEventListener = webSocketEventListener;
+        this.dataConsumptionRepository = dataConsumptionRepository;
     }
 
     public void initialize() {
@@ -35,17 +32,9 @@ public class GetConsumptionDataService {
             @Override
             public void run() {
                 sendConsumptionDataToWS();
-                int newInterval = generateRandomInterval(MIN_INTERVAL_VALUE, MAX_INTERVAL_VALUE);
-                timer.schedule(this, newInterval);
             }
         };
-
-        timer.schedule(task, 0);
-    }
-
-    private static int generateRandomInterval(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(min - max + 1) + min;
+        timer.scheduleAtFixedRate(task, 0, VERIFY_DATABASE_RATE);
     }
 
     private void sendConsumptionDataToWS() {
