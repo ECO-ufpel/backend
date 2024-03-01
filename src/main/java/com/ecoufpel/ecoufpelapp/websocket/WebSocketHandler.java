9ab95e5f39d36b5c3d432730aa45fc441333ae16
JelsonRodrigues <jelsonrodrigues25@gmail.com>
2024-03-01 17:44:43 -0300

    feat: support for messaging sub/unsub to rooms

--- src/main/java/com/ecoufpel/ecoufpelapp/websocket/WebSocketHandler.java
+++ src/main/java/com/ecoufpel/ecoufpelapp/websocket/WebSocketHandler.java
@@ -1,10 +1,13 @@
 package com.ecoufpel.ecoufpelapp.websocket;
 
 import com.ecoufpel.ecoufpelapp.domains.user.User;
+import com.ecoufpel.ecoufpelapp.domains.websocket.SubscriptionToRoomMessageDTO;
+import com.ecoufpel.ecoufpelapp.domains.websocket.SubscriptionToRoomMessageDTO;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -27,8 +30,28 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        var optionalSubscribeMessage = SubscriptionToRoomMessageDTO.fromJson(message.getPayload());
+
+        if (optionalSubscribeMessage.isEmpty()) {
+            System.out.println("Invalid message");
+            return;
+        }
+
+        var subscribeMessage = optionalSubscribeMessage.get();
         System.out.println("Received message: " + message.getPayload());
-        session.sendMessage(new TextMessage("Polo!"));
+
+        switch (subscribeMessage.type()) {
+            case SUBSCRIBE -> subscribeToRoom(session, subscribeMessage);
+            case UNSUBSCRIBE -> unsubscribeFromRoom(session, subscribeMessage);
+        }
+    }
+
+    public void subscribeToRoom(WebSocketSession session, SubscriptionToRoomMessageDTO message) {
+        webSocketEventListener.register_user(message.room_id(), session);
+    }
+
+    public void unsubscribeFromRoom(WebSocketSession session, SubscriptionToRoomMessageDTO message) {
+        webSocketEventListener.remove_user(message.room_id(), session);
     }
 
     @Override
@@ -45,11 +68,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
-        User user = (User) session.getAttributes().get("user");
-        if (user == null) {
-            return;
-        }
-        webSocketEventListener.remove_user(user, session);
+        webSocketEventListener.remove_user(session);
     }
 
 }
