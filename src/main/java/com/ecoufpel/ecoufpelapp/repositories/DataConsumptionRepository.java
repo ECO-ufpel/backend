2164566169a3522ad3db34b4ec03559a0cb77bf9
Allan <allan20.1@hotmail.com>
2024-02-29 21:07:55 -0300

    feat: create service to get consumption data from database and send to WS

--- src/main/java/com/ecoufpel/ecoufpelapp/repositories/DataConsumptionRepository.java
+++ src/main/java/com/ecoufpel/ecoufpelapp/repositories/DataConsumptionRepository.java
@@ -4,7 +4,11 @@ import com.ecoufpel.ecoufpelapp.domains.sensor.DataConsumption;
 import com.ecoufpel.ecoufpelapp.domains.sensor.DataConsumptionID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DataConsumptionRepository extends JpaRepository<DataConsumption, DataConsumptionID> {
+    Optional<List<DataConsumption>> findById_DateTimeBetween(Timestamp startTimestamp, Timestamp endTimestamp);
 }
---
+++ src/main/java/com/ecoufpel/ecoufpelapp/services/GetConsumptionDataService.java
@@ -0,0 +1,65 @@
+package com.ecoufpel.ecoufpelapp.services;
+
+
+import com.ecoufpel.ecoufpelapp.domains.sensor.DataConsumptionDTO;
+import com.ecoufpel.ecoufpelapp.websocket.WebSocketEventListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ecoufpel.ecoufpelapp.repositories.DataConsumptionRepository;
+import java.sql.Timestamp;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Service
+public class GetConsumptionDataService {
+
+    public static final int MIN_INTERVAL_VALUE = 3000;
+    public static final int MAX_INTERVAL_VALUE = 10000;
+
+    @Autowired
+    private WebSocketEventListener webSocketEventListener;
+
+    @Autowired
+    DataConsumptionRepository dataConsumptionRepository;
+    private Timestamp lastTimeStampChecked;
+    @Autowired
+    public GetConsumptionDataService(WebSocketEventListener webSocketEventListener) {
+        this.webSocketEventListener = webSocketEventListener;
+    }
+
+    public void initialize() {
+        lastTimeStampChecked = new Timestamp(System.currentTimeMillis());
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                sendConsumptionDataToWS();
+                int newInterval = generateRandomInterval(MIN_INTERVAL_VALUE, MAX_INTERVAL_VALUE);
+                timer.schedule(this, newInterval);
+            }
+        };
+
+        timer.schedule(task, 0);
+    }
+
+    private static int generateRandomInterval(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(min - max + 1) + min;
+    }
+
+    private void sendConsumptionDataToWS() {
+        dataConsumptionRepository
+            .findById_DateTimeBetween(lastTimeStampChecked, new Timestamp(System.currentTimeMillis()))
+            .ifPresent(dataConsumptionList -> {
+                dataConsumptionList.forEach(dataConsumptionEntry -> {
+                    webSocketEventListener.onNext(new DataConsumptionDTO(
+                            dataConsumptionEntry.getId().getClassroomId(),
+                            dataConsumptionEntry.getId().getDateTime(),
+                            dataConsumptionEntry.getConsumption()
+                            )
+                    );
+                });
+            });
+    }
+}
