9cb19f47bd68d09ba3d0371948a47a99a734a07a
Allan <allan20.1@hotmail.com>
2024-02-29 21:06:45 -0300

    feat: add @Getter on DataConsumptionID

--- src/main/java/com/ecoufpel/ecoufpelapp/domains/sensor/DataConsumptionID.java
+++ src/main/java/com/ecoufpel/ecoufpelapp/domains/sensor/DataConsumptionID.java
@@ -2,12 +2,14 @@ package com.ecoufpel.ecoufpelapp.domains.sensor;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Embeddable
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DataConsumptionID implements Serializable {
