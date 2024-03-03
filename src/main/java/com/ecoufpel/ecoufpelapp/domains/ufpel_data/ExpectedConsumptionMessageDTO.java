c74ed56cba5d8d01acb68e315ad16776cf81096b
JelsonRodrigues <jelsonrodrigues25@gmail.com>
2024-03-03 09:40:09 -0300

    feat: send message of expected consumption for room on subscribe

---
+++ src/main/java/com/ecoufpel/ecoufpelapp/domains/ufpel_data/ExpectedConsumptionMessageDTO.java
@@ -0,0 +1,16 @@
+package com.ecoufpel.ecoufpelapp.domains.ufpel_data;
+
+import org.springframework.web.socket.TextMessage;
+
+public record ExpectedConsumptionMessageDTO(String roomId, Double expectedConsumption, Double stdDev) {
+    public TextMessage toTextMessage() {
+        return new TextMessage(String.format("""
+                {
+                "type": "expected_consumption",
+                "average": "%s",
+                "std_dev": "%s",
+                "room_id": "%s"
+                }
+                """, expectedConsumption, stdDev, roomId));
+    }
+}
